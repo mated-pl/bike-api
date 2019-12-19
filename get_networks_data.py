22221bf0f088478033ffb101ba1d70a72697d37a
mated <elmatte@gmail.com>
2019-12-19 16:53:19 +0100

    get the response convert to json convert to python object search for country list cities from that country

--- get_networks_data.py
+++ get_networks_data.py
@@ -3,11 +3,11 @@ import requests, json
 response1 = requests.get("http://api.citybik.es/v2/networks")
 print(response1.status_code)
 
-def format_response (x):                         # creates json from a response
+def response_to_json (x):                         # creates json from a response
     formatted_response = json.dumps(x, sort_keys=True, indent=3)
     return(formatted_response)
 
-response_json = format_response(response1.json())
+response_json = response_to_json(response1.json())
 
 
 response2dict = json.loads(response_json)        # converts json to dict
@@ -17,15 +17,18 @@ networks = response2dict["networks"]            # all data in the response sits 
 #     print(networks[n]['company'])
 
 # print(networks.items())                       # prints whole dictionary
-
 # print(networks[0]['location']['country'])       # prints: 1st item in networks, value for location, value for country
 
-HR_list =  []
-for n in range (0,len(networks)):               # range from 0 to the last element
-    if networks[n]['location']['country'] == "HR":
-        HR_list.append(networks[n]['location']['city'])
-# print(HR_list)                                # prints as a list
-print('\n'.join(HR_list))                       # prints as a string in next rows
 
 
 #TODO: zrob tu porzadek - funkcje. Merge to master, push. Zaloz nowy branch na zapisywanie do pliku
+
+def country_filter(resp_dict, country):
+    city_list = []
+    for n in range (0,len(resp_dict)):                          # range from 0 to the last element
+        if networks[n]['location']['country'] == country:
+            city_list.append(networks[n]['location']['city'])
+    print('\n'.join(city_list))                                 # prints as a string in next rows
+    return (city_list)
+
+country_filter(networks,"PL")
